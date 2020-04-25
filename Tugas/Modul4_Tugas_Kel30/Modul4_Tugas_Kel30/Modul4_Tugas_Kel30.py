d9572dea2a612c1e000db7fbdf8f40df27941de2
ilhamdaffa21 <64187539+ilhamdaffa21@users.noreply.github.com>
2020-04-25 21:51:53 +0700

    Update function confirm pw

--- Tugas/Modul4_Tugas_Kel30/Modul4_Tugas_Kel30/Modul4_Tugas_Kel30.py
+++ Tugas/Modul4_Tugas_Kel30/Modul4_Tugas_Kel30/Modul4_Tugas_Kel30.py
@@ -1,30 +1,34 @@
 import metod
-print("Buat passsword User 1")
-pw1 = input()
-print("Buat password User 2")
-pw2 = input()
+def buatpassword ():
+        opin=0
+        opin2=1
+        knt=0
+        while opin != opin2:
+            if knt > 0:
+               print("Password dan Password Konfirmasi Berbeda")
+            knt=1
+            print("Masukkan password : ")
+            opin = input()
+            print("Masukkan password konfirmasi : ")
+            opin2 = input()
+        return(opin)
+        print("Login berhasil")
+
+print("Buat Password 1")
+pin1=buatpassword()
+print("Buat Password 2")
+pin2=buatpassword()
+
 print("Login")
-print("User1(1)")
-print("User2(2)")
+print("User 1(1)")
+print("User 2(2)")
 print("Input 1 atau 2")
 user = int(input())
+p = metod.huhaha(user, pin1, pin2)
 if user == 1:
-    print("Masukkan password User1")
-    pw = input()
-    while pw != pw1:
-            print("Password salah")
-            print("Masukkan password")
-            pw = input()
-            print("Login berhasil")
+    p.ulang1()
     
 else:
-    print("Masukkan password User2")
-    pw = input()
-    while pw != pw2:
-            print("Password salah")
-            print("Masukkan password")
-            pw = input()
-            print("Login berhasil")
-p = metod.huhaha("user")
+    p.ulang2()
 p.mulai()
 p.selesai(10)
